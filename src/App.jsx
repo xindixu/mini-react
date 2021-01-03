3f25236fc968b827c783d458eb71049fd088f211
Xindi Xu <xindixu@utexas.edu>
2021-01-03 18:42:12 -0500

    chore/setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React from "./x-react/react";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <h1>React</h1>
+      <p>is pretty cool</p>
+    </div>
+  );
 }
 
 export default App;
